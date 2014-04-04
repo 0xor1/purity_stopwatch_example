0b8d2cf31cc7c6d0dd0eac2f99d5a799b5955137
Daniel Robinson <daniel.robinson.open@gmail.com>
2014-04-04 14:21:53 +0100

    update to latest purity with closeApp arg

--- bin/server.dart
+++ bin/server.dart
@@ -9,5 +9,10 @@ import 'package:purity/purity_server.dart';
 import 'package:purity_stopwatch_example/model/stopwatch.dart' as SW;
 
 void main(){
-  var server = new PurityServer(InternetAddress.LOOPBACK_IP_V4, 4346, Platform.script.resolve('../build/web').toFilePath(), () => new SW.Stopwatch());
+  var server = new PurityServer(
+    InternetAddress.LOOPBACK_IP_V4,
+    4346,
+    Platform.script.resolve('../build/web').toFilePath(),
+    () => new SW.Stopwatch(),
+    (stopwatch){});
 }
