7832f714432fedf389d0e1e2f8a445eb967327c1
0xor1 <daniel.robinson.open@gmail.com>
2014-07-18 15:24:38 +0100

    remove all unnecessary build files

--- packages/purity/src/core/tran/garbage_collection_start.dart
+++
@@ -1,7 +0,0 @@
-/**
- * Author:  Daniel Robinson http://github.com/0xor1
- */
-
-part of purity.core;
-
-class _GarbageCollectionStart extends _Transmission{}
--- packages/purity/src/core/tran/transmission.dart
+++
@@ -1,7 +0,0 @@
-/**
- * Author:  Daniel Robinson http://github.com/0xor1
- */
-
-part of purity.core;
-
-abstract class _Transmission extends Transmittable{}
